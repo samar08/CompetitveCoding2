7e273cf10ec1fb3d3532bdacedee5326a39c6a8b
samar08 <samarasimhareddy.mohan@gmail.com>
2022-05-17 23:51:53 +0530

    linkedlist

---
+++ LinkedList/Reverse_a_linkedlist.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public ListNode reverseList(ListNode A) {
+        ListNode curr=A;
+        ListNode prev=null;
+        ListNode next=null;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
