48f1506b5b3a36ca9b5a6a49231fad146fdff499
samarhpe <96163858+samarhpe@users.noreply.github.com>
2023-03-03 09:03:17 +0530

    updated heap_sort.java

--- Heaps/Heap_sort.java
+++ Heaps/Heap_sort.java
@@ -13,47 +13,38 @@ class Solution {
     }
     public void heapify(int[] nums,int st, int len){
         //int n=nums.length;
-        int i=st,smallest=st;
+        int i=st,largest=st;
         int left=2*i+1;
         int right=2*i+2;
-        if(left<len && (nums[i]>nums[left])){
-            if((right < len) && nums[right]<nums[left]){
+        if(left<len && (nums[i]<nums[left])){
+            if((right < len) && nums[right]>nums[left]){
                 int temp=nums[right];
                 nums[right]=nums[i];
                 nums[i]=temp;
-                smallest=right;
+                largest=right;
             }
             else{
                 int temp=nums[left];
                 nums[left]=nums[i];
                 nums[i]=temp;
-                  smallest=left;
+                  largest=left;
             }
         }
-        else if(right<len && (nums[i]>nums[right])){
+        else if(right<len && (nums[i]<nums[right])){
             int temp=nums[right];
                 nums[right]=nums[i];
                 nums[i]=temp;
-              smallest=right;
+              largest=right;
         }
-        if(smallest!=st){
-            heapify(nums,smallest,len);
+        if(largest!=st){
+            heapify(nums,largest,len);
         }
 
     }
-    public void reverse(int[] nums,int n){
-        int i;
-        for(i=0;i<(int)(n/2);i++){
-            int temp= nums[n-i-1];
-            nums[n-i-1]=nums[i];
-            nums[i]=temp;
-        }
-    }
+   
     public int[] sortArray(int[] nums) {
         int i=0,n=nums.length;
         heapsort(nums,n);
-        reverse(nums,n);
-
         return nums;
 
     }
