094e858913e695b26461be98f0d4ce04c89c9bc9
samar08 <samarasimhareddy.mohan@gmail.com>
2024-01-12 23:24:04 +0530

    exception handling practise

--- Practise/ExceptionsPractise.java
+++ Practise/ExceptionsPractise.java
@@ -1,13 +1,31 @@
-import java.io.*;
 import java.util.*;
-public class ExceptionsPractise {
-    
-    public static void main(String[] args) throws FileNotFoundException{
-        File myObj = new File("./Practise/sample.txt");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
+public class Main
+{
+    public static void divide() throws ArithmeticException{
+        System.out.println("divide called");
+       sampleFunction();
     }
-}
+    public static void sampleFunction() throws ArithmeticException{
+          System.out.println("sampleFunction called");
+        orgFunction();
+    }
+    public static void orgFunction() throws ArithmeticException{
+          System.out.println("orgFunction called");
+        int res= 10/0;
+         //throw new ArithmeticException("arithmetic exception called");
+    }
+	public static void main(String[] args) {
+	    try{
+	        divide();
+	    }
+	    catch(Exception e){
+	        System.out.println(e.getMessage());
+	    }
+	   //File myObj = new File("filename.txt");
+    //   Scanner myReader = new Scanner(myObj);
+    //   while (myReader.hasNextLine()) {
+    //     String data = myReader.nextLine();
+    //     System.out.println(data);
+		
+	}
 }
