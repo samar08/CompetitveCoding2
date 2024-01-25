7670251c76b9e3b806aadca24f4f536cdcbb373a
samar08 <samarasimhareddy.mohan@gmail.com>
2024-01-25 09:30:02 +0530

    move zeros another solution added

--- Arrays/MoveZeros.py
+++ Arrays/MoveZeros.py
@@ -18,7 +18,40 @@ class Solution:
             i+=1
         for i in range(count,n):
             nums[i]=0
+
+'''
+Another solution:
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i,n=nums.length,first=0,countzeros=0;
+        for(i=0;i<n;i++){
+            if(nums[i]==0){
+                countzeros+=1;
+            }
+        }
         
+        i=0;
+        while(i<n-countzeros){
+            if(nums[i]==0){
                 
+                 first=i;
+                while(first<n && nums[first]==0){
+                    first++;
+                }
+                int temp=nums[i];
+                nums[i]=nums[first];
+                nums[first]=temp;
+               while(first<n && nums[first]==0){
+                    first++;
+                }
+            }
+            else{
+                i++;
+            }
+        }
+
+    }
+}
+'''           
         
         
