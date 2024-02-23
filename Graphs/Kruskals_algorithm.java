7e81b93eb864ae1b4dc8bf0c9e338e2590dd4333
samar08 <samarasimhareddy.mohan@gmail.com>
2024-02-23 11:42:12 +0530

    modified topological sort

--- Graphs/Kruskals_algorithm.java
+++ Graphs/Kruskals_algorithm.java
@@ -192,5 +192,5 @@ class Graph {
 		graph.KruskalMST();
 	}
 }
-// This code is contributed by Aakash Hasija
+
 
--- Graphs/Topologicalsort.java
+++ Graphs/Topologicalsort.java
@@ -1,28 +1,28 @@
 public class Topologicalsort {
-    static void dfs(int i,boolean[]vis,ArrayList<ArrayList<Integer>> adj,Stack<Integer> st){
+    static void dfs(int i,boolean[]vis,ArrayList<ArrayList<Integer>> adj,Stack<Integer> stack){
         vis[i]=true;
         for(int v:adj.get(i)){
             if(vis[v]==false){
-                dfs(v,vis,adj,st);
+                dfs(v,vis,adj,stack);
             }
         }
-        st.push(i);
+        stack.push(i);
         return;
     }
     static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
     {
         boolean[]vis=new boolean[V];
         int i;
-        Stack<Integer>st=new Stack<Integer>();
+        Stack<Integer>stack=new Stack<Integer>();
         for(i=0;i<V;i++){
             if(vis[i]==false){
-                dfs(i,vis,adj,st);
+                dfs(i,vis,adj,stack);
             }
         }
         int[]out=new int[V];
         int k=0;
-        while(st.isEmpty()==false){
-            out[k]=st.pop();
+        while(stack.isEmpty()==false){
+            out[k]=stack.pop();
             k+=1;
         }
         return out;
