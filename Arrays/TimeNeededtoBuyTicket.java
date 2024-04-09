74ce36f8399f201c4ea1dd034f1c0545ff1ad638
samar08 <samarasimhareddy.mohan@gmail.com>
2024-04-09 13:00:18 +0530

    O(n) solution for Time needed to buy ticket problem

--- Arrays/TimeNeededtoBuyTicket.java
+++ Arrays/TimeNeededtoBuyTicket.java
@@ -12,3 +12,24 @@ class Solution {
         return time;
     }
 }
+/* O(n) solution */
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int i,n=tickets.length,time=0;
+        
+        for(i=0;i<n;i++){
+            if(i<=k){
+            time+=Math.min(tickets[i],tickets[k]);
+            }
+            else{
+                if(tickets[i]>=tickets[k]){
+                    time+=tickets[k]-1;
+                }
+                else{
+                    time+=tickets[i];
+                }
+            }
+        }
+        return time;
+    }
+}
