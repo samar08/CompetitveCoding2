6c0a793845f52bd3f646f249c86ddb230ea3f491
samar08 <samarasimhareddy.mohan@gmail.com>
2024-04-15 13:40:20 +0530

    sum of left leaves

--- Trees/Sum_of_left_leaves.java
+++ Trees/Sum_of_left_leaves.java
@@ -1,54 +1,23 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public int sumofleftleaves(TreeNode node){
-        if(node!=null){
-            int count=0;
-            if(node.left!=null){
-                TreeNode left=node.left;
-                if(left.left==null && left.right==null){
-                    count+=left.val;
-                }
-                else{
-                    count+=sumofleftleaves(node.left);
-                }
-            }
-            if(node.right!=null){
-                 count+=sumofleftleaves(node.right);
-            }
-            return count;
-        }
-        else{
+    public int sumOfLeftLeaves(TreeNode root) {
+      
+        if(root==null){
             return 0;
         }
-    }
-    public int sumOfLeftLeaves(TreeNode root) {
-        int count=0;
+        int sum=0,i;
         if(root.left!=null){
-            TreeNode left=root.left;
-            if(left.left==null && left.right==null){
-                count+=left.val;
+            if(root.left.right==null && root.left.left==null){
+                sum+=root.left.val;
             }
-            else{
-                 count+=sumofleftleaves(left);
-            }
+            
+        }
+        if(root.left!=null){
+            sum+=sumOfLeftLeaves(root.left);
         }
         if(root.right!=null){
-            count+=sumofleftleaves(root.right);
+            sum+=sumOfLeftLeaves(root.right);
         }
-        return count;
+        return sum;
+        
     }
 }
